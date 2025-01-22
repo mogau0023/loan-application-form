4ad0c160603723c43397e85cb1b9978716e3050f
mogau stanley mphekgwana <mogau0023@gmail.com>
2025-01-22 17:02:12 +0200

    Add config.js with Firebase config

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyApig9HbRt8AVt1jZvNqRhImP3QbALMlTU",
+  authDomain: "uni-loan-bd731.firebaseapp.com",
+  projectId: "uni-loan-bd731",
+  storageBucket: "uni-loan-bd731.firebasestorage.app",
+  messagingSenderId: "935736429714",
+  appId: "1:935736429714:web:97d1d3339092ca88f82a4b",
+  measurementId: "G-ZBML53LNDR"
+};
